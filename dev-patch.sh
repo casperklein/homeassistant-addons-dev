63138744cdb666a134e34f9f19d55ebbaa3f94bc
casperklein <casperklein@users.noreply.github.com>
2025-07-06 21:45:02 +0200

    update

--- dev-patch.sh
+++ dev-patch.sh
@@ -34,13 +34,19 @@ sedfile -i 's|2Fhomeassistant-addons|2Fhomeassistant-addons-dev|g' README.md
 
 
 sedfile -i "s|$1|$1-dev|g" README.md
-sedfile -i 's|-dev%2Fconfig.json|%2Fconfig.json|g' README.md # Fix version shield
+#sedfile -i 's|-dev%2Fconfig.json|%2Fconfig.json|g' README.md # Fix version shield
+sedfile -i "s|master%2F$1-dev|master%2F$1|g" README.md # Fix version shield
+
+# Only for pi-hole-dhcp-helper
+if [ "$1" == "pi-hole-dhcp-helper" ]; then
+	sedfile -i "s|master/$1-dev|master/$1|g"     README.md # Fix embedded images
+fi
 
 IDENTIFIER_STABLE=$(printf %s https://github.com/casperklein/homeassistant-addons  | sha1sum | head -c8)
 IDENTIFIER_DEV=$(printf %s https://github.com/casperklein/homeassistant-addons-dev | sha1sum | head -c8)
 sedfile -i "s|$IDENTIFIER_STABLE|$IDENTIFIER_DEV|g" README.md
 
-# Only for pihole:
+# Only for pi-hole, because HA slug differs from directory name (legacy)
 if [ "$1" == "pi-hole" ]; then
 	sedfile -i -E 's|_pihole|_pihole-dev|g' README.md
 fi
